60561057514470426c89d2171c0b688efe4766a4
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2021-12-11 22:49:03 +0300

    name/mail/password/address
    sayfalar eklendi

---
+++ mobileApp/app/src/main/java/com/example/silverarrowmobileapp/showCars.java
@@ -0,0 +1,14 @@
+package com.example.silverarrowmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showCars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_cars);
+    }
+}
