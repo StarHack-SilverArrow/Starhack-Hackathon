caf58a290b8da723ab26198a32149f71f53520f1
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2021-12-11 17:18:52 +0300

    activity_main
    activity_main layout

--- mobileApp/app/src/main/java/com/example/silverarrowmobileapp/MainActivity.java
+++ mobileApp/app/src/main/java/com/example/silverarrowmobileapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.silverarrowmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void SignUp(View view) {
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
