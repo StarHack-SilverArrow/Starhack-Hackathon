0437d763e985df5d9134860b011f10c62c2a0f78
Mehmet Arif Bağcı <arif.bagci@hotmail.com>
2021-12-11 23:54:53 +0300

    signup 1/3 backend is completed.

--- mobileApp/app/src/main/java/com/example/silverarrowmobileapp/MainActivity.java
+++ mobileApp/app/src/main/java/com/example/silverarrowmobileapp/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.silverarrowmobileapp.Model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -19,43 +23,33 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth; // Hepsini  singleton depolamaya tasimaya calis
+    private static final String TAG = "EmailPassword";
     private FirebaseUser user;
+    private FirebaseFirestore firebaseFirestore;
     public static User mainUser;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        user = firebaseAuth.getCurrentUser();
-        //Bu kisimda yapmam gerekmez muhtemelen  giris ekraninda kontrol edilir zaten
-        if (user != null) {
-            DocumentReference docRef = FirebaseFirestore.getInstance().collection("cities").document(user.getUid());
-            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    mainUser =  documentSnapshot.toObject(User.class);
-                }
-            });
-            System.out.println("test");
-        }
-        else {
-            System.out.println("baglanmadi");
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore =  FirebaseFirestore.getInstance();;
 
-        MatchGenerator.getAllMatch();
+        findViewById(R.id.buttoncontinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+                finish();
+
+            }
+        });
     }
 
-    public void login(View view) {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-    public void SignUp(View view) {
-        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-        startActivity(intent);
-    }
 }
--- mobileApp/app/src/main/java/com/example/silverarrowmobileapp/SignUpActivity.java
+++ mobileApp/app/src/main/java/com/example/silverarrowmobileapp/SignUpActivity.java
@@ -1,14 +1,71 @@
 package com.example.silverarrowmobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private FirebaseFirestore firebaseFirestore;
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+       // ...
+// Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore =  FirebaseFirestore.getInstance();;
+        EditText mail = findViewById(R.id.editTextMail);
+        EditText password = findViewById(R.id.editTextTextPassword);
+        Button goOn = findViewById(R.id.goOn);
+        goOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createAccount(mail.getText().toString(), password.getText().toString());
+                Intent intent = new Intent(SignUpActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void createAccount(String mail, String password) {
+
+        firebaseAuth.createUserWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            // [START create_user_with_email]
+                            Toast.makeText(SignUpActivity.this, firebaseAuth.getUid(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+    
+
+
 }
