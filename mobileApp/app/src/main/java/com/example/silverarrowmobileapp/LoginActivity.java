7f718674cb6115887da2b905c52cb3d68cbdfca4
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2021-12-12 07:35:48 +0300

    Car Screen arranged

--- mobileApp/app/src/main/java/com/example/silverarrowmobileapp/LoginActivity.java
+++ mobileApp/app/src/main/java/com/example/silverarrowmobileapp/LoginActivity.java
@@ -17,11 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.annotations.NotNull;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
